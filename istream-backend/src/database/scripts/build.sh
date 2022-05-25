0b5aebfdd0a0d29826d025a28666a97b38026af4
Navid-ak <anavid.akbari@gmail.com>
2022-05-25 17:14:25 -0600

    uncomment building and running phase scripts

--- istream-backend/src/database/scripts/build.sh
+++ istream-backend/src/database/scripts/build.sh
@@ -9,25 +9,25 @@ networkComponentExistence=$(jq '.networkComponentExistence' <<<${experimentsList
 
 # Video Component
 echo "------ Video component building started ------"
-# sh src/database/scripts/Video/build.sh "${username}" "${experimentId}" 2>&1
+sh src/database/scripts/Video/build.sh "${username}" "${experimentId}" 2>&1
 echo "------ Video component building Finished ------"
 
 # Server Component
 echo "------ Server component building started ------"
-# sh src/database/scripts/Server/build.sh "${username}" "${experimentId}" 2>&1
+sh src/database/scripts/Server/build.sh "${username}" "${experimentId}" 2>&1
 echo "------ Server component building Finished ------"
 
 # Transcoder Component
 if [ ${transcoderComponentExistence} = true ]; then
     echo "------ Transcoder component building started ------"
-    # sh src/database/scripts/Transcoder/build.sh "${username}" "${experimentId}" 2>&1
+    sh src/database/scripts/Transcoder/build.sh "${username}" "${experimentId}" 2>&1
     echo "------ Transcoder component building Finished ------"
 fi
 
 # Network Component
 if [ ${networkComponentExistence} = true ]; then
     echo "------ Network component building started ------"
-    # sh src/database/scripts/Network/build.sh "${username}" "${experimentId}" 2>&1
+    sh src/database/scripts/Network/build.sh "${username}" "${experimentId}" 2>&1
     echo "------ Network component building Finished ------"
 fi
 
--- istream-backend/src/database/scripts/run.sh
+++ istream-backend/src/database/scripts/run.sh
@@ -9,20 +9,20 @@ networkComponentExistence=$(jq '.networkComponentExistence' <<<${experimentsList
 
 # Server Component
 echo "------ Server component running started ------"
-# sh src/database/scripts/Server/run.sh "${username}" "${experimentId}" 2>&1
+sh src/database/scripts/Server/run.sh "${username}" "${experimentId}" 2>&1
 echo "------ Server component running Finished ------"
 
 # Transcoder Component
 if [ ${transcoderComponentExistence} = true ]; then
     echo "------ Transcoder component running started ------"
-    # sh src/database/scripts/Transcoder/run.sh "${username}" "${experimentId}" 2>&1
+    sh src/database/scripts/Transcoder/run.sh "${username}" "${experimentId}" 2>&1
     echo "------ Transcoder component running Finished ------"
 fi
 
 # Network Component
 if [ ${networkComponentExistence} = true ]; then
     echo "------ Network component running started ------"
-    # sh src/database/scripts/Network/run.sh "${username}" "${experimentId}" 2>&1
+    sh src/database/scripts/Network/run.sh "${username}" "${experimentId}" 2>&1
     echo "------ Network component running Finished ------"
 fi
 
