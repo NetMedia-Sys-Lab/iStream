8d2d7dcf1fff56f712be6680beded7dc5bed8785
Navid-ak <anavid.akbari@gmail.com>
2022-06-13 11:21:18 -0700

    Fix a bug in transferring video script dataset to other places

--- istream-backend/src/database/scripts/Video/build.sh
+++ istream-backend/src/database/scripts/Video/build.sh
@@ -11,8 +11,11 @@ videosName=()
 for i in "${!videoId[@]}"; do
     videosList=$(jq --arg videoId ${videoId[$i]} '.[] | select(.id == $videoId)' src/database/users/${username}/Videos/videos_list.json)
     videoName=$(jq '.name' <<<${videosList})
-    videosName+=($(echo ${videoName} | tr -d '"'))
+    extensions+=($(echo ${videoName##*.} | tr -d '"'))
+    videosName+=("$(echo ${videoName} | tr -d '"')")
 done
+
+echo ${videosName[2]}
 
 serverName=$(jq -r '.Server.name' src/database/users/${username}/Experiments/${experimentId}/dependency.json)
 serverType=$(jq -r '.Server.type' src/database/users/${username}/Experiments/${experimentId}/dependency.json)
@@ -25,22 +28,22 @@ if [[ "${videoMachineId}" != "" ]] && [[ "${videoMachineId}" != "0" ]]; then
 
     for i in "${!videoId[@]}"; do
         echo "Move video number $(($i + 1)) to the designated machine"
-        videoPath="${mainDir}/src/database/users/${username}/Videos/${videoId[$i]}.${videosName[$i]##*.}"
-        sh src/database/scripts/Common/scp.sh "${sshUsername}" "${machineIp}" "${privateKeyPath}" "${videoPath}" "video" ${videosName[$i]}
+        videoPath="${mainDir}/src/database/users/${username}/Videos/${videoId[$i]}.${extensions[$i]}"
+        sh src/database/scripts/Common/scp.sh "${sshUsername}" "${machineIp}" "${privateKeyPath}" "${videoPath}" "video" "${videosName[$i]}"
     done
 fi
 
 if [[ "${serverType}" == "iStream" ]]; then
     for i in "${!videoId[@]}"; do
         echo "Move video number $(($i + 1)) beside server component"
-        videoPath="${mainDir}/src/database/users/${username}/Videos/${videoId[$i]}.${videosName[$i]##*.}"
+        videoPath="${mainDir}/src/database/users/${username}/Videos/${videoId[$i]}.${extensions[$i]}"
         serverPath="${mainDir}/src/database/supportedModules/Server/${serverName}/Build/${videosName[$i]}"
         cp "${videoPath}" "${serverPath}"
     done
 elif [[ "${serverType}" == "Custom" ]]; then
     for i in "${!videoId[@]}"; do
         echo "Move video number $(($i + 1)) beside server component"
-        videoPath="${mainDir}/src/database/users/${username}/Videos/${videoId[$i]}.${videosName[$i]##*.}"
+        videoPath="${mainDir}/src/database/users/${username}/Videos/${videoId[$i]}.${extensions[$i]}"
         serverPath="${mainDir}/src/database/users/${username}/Modules/Server/${serverName}/Build/${videosName[$i]}"
         cp "${videoPath}" "${serverPath}"
     done
