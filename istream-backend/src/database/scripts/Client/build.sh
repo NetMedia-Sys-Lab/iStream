fc21e17cc7bd4cedaf002e59e2f3d8aba7b6f820
Navid-ak <anavid.akbari@gmail.com>
2022-12-19 20:43:45 -0700

    Fix path in scripts

--- istream-backend/src/database/scripts/Client/build.sh
+++ istream-backend/src/database/scripts/Client/build.sh
@@ -4,13 +4,13 @@ experimentId=$2
 
 mainDir=$(pwd)
 
-clientName=$(jq -r '.Client.name' "${mainDir}src/database/users/${username}/Experiments/${experimentId}/dependency.json")
-clientType=$(jq -r '.Client.type' "${mainDir}src/database/users/${username}/Experiments/${experimentId}/dependency.json")
-clientMachineId=$(jq -r '.Client.machineID' "${mainDir}src/database/users/${username}/Experiments/${experimentId}/dependency.json")
+clientName=$(jq -r '.Client.name' "${mainDir}/src/database/users/${username}/Experiments/${experimentId}/dependency.json")
+clientType=$(jq -r '.Client.type' "${mainDir}/src/database/users/${username}/Experiments/${experimentId}/dependency.json")
+clientMachineId=$(jq -r '.Client.machineID' "${mainDir}/src/database/users/${username}/Experiments/${experimentId}/dependency.json")
 
 if [[ "${clientName}" == "" ]]; then
     echo "No client module selected. Please select a module first."
     exit
 else
-    sh "${mainDir}src/database/scripts/Common/build.sh" "${username}" "Client" "${clientName}" "${clientType}" "${clientMachineId}"
+    sh "${mainDir}/src/database/scripts/Common/build.sh" "${username}" "Client" "${clientName}" "${clientType}" "${clientMachineId}"
 fi
